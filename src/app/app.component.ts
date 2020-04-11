eeec79caf1c2bbceebcaaaa97639c6a7e3ac2680
frederickinerk <dennis.j.higgins@gmail.com>
2020-04-11 15:34:27 +1000

    Small tidy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Higgins Clan Birthdays';
+  title = 'Birthdays';
   todayStr;
 
   ngOnInit() {
     var d = new Date();
     var str: string;
 
-    str = d.toISOString()//d.toLocaleDateString(Intl.DateTimeFormat)
-    this.todayStr = str; //d.toLocaleDateString();
+    str = d.toLocaleDateString();
+    this.todayStr = str; 
+  }
+
+  getImageName(){
+    return("./assets/images/Banner.png")
+
+    //<img src="https://s3-ap-southeast-2.amazonaws.com/higgins.one/birthdays/assets/images/Banner.png" alt="banner">
   }
 }
